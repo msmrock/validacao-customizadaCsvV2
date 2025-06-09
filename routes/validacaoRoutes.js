f9608efd2122536c3af0db96f1073903473043ce
Mario Mainardes <75805447+msmrock@users.noreply.github.com>
2025-06-09 13:18:07 -0300

    Update validacaoRoutes.js
    nova versao

--- routes/validacaoRoutes.js
+++ routes/validacaoRoutes.js
@@ -1,8 +1,18 @@
-
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const validacaoController = require('../controllers/validacaoController');
+const validacaoController = require("../controllers/validacaoController");
 
-router.post('/', validacaoController.validarCracha);
+// Função de log personalizada com timestamp
+function logInfo(message) {
+  const timestamp = new Date().toLocaleString("pt-BR");
+  console.log(`[${timestamp}] [INFO] ${message}`);
+}
+
+// Rota de validação
+router.post("/", (req, res, next) => {
+  logInfo("Rota POST /validacao-customizadaCsv acionada");
+  validacaoController.validarCracha(req, res, next);
+});
 
 module.exports = router;
+
